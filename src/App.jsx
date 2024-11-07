04d2c84381c4631c4b9004196a22483f24c7390e
ALI9ISSA9 <ali7333170@gmail.com>
2024-11-07 15:56:33 +0200

    added an input with its basic styling

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-container">
+      <h1>Search</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-input"
+      />
+    </div>
+  );
+}
+
+export default App;
